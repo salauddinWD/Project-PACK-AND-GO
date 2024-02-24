b3b9633968bba2a35b60572fccbbf37320275f2e
Md. Salauddin <salauddin17001@gmail.com>
2024-02-24 19:51:35 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+$('.dest-body').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1500,
+    centerMode: true,
+    centerPadding: "0",
+  });
+      
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+
+$('.testslider_body').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 2000,
+  dots: true,
+});
+	
+
